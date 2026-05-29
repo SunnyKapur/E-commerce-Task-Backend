97ab2f3cc75fd37ca3cce9f1267ea0b83db5e634
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-05-30 01:24:21 +0530

    auth routes setup

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from "express";
 import productRoutes from "./routes/product.route.js"
-
+import authRoutes from './routes/auth.routes.js'
 
 let app = express()
 app.use(express.json())
 
 app.use('/api', productRoutes)
+app.use('/api/auth', authRoutes)
 
 export default app
