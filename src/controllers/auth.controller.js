3b68a1593c30a87b27964b80a10d7ab6843b4d43
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-05-30 02:18:58 +0530

    Implement user authentication with register and login APIs

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -40,7 +40,52 @@ export let registerController = async (req, res) => {
 
     return res.status(201).json({
       message: "User registered successfully",
-      user
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export let loginController = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+
+    if (!email || !password)
+      return res.status(400).json({
+        message: "Email and Password are required",
+      });
+
+    let isExisted = await UserModel.findOne({ email });
+
+    if (!isExisted)
+      return res.status(404).json({
+        message: "User not found",
+      });
+
+    let comparePass = await bcrypt.compare(password, isExisted.password);
+
+    if (!comparePass)
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+
+    let token = jwt.sign({ id: isExisted._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    res.cookie("id_card", token,{
+      httpOnly: true
+    });
+
+    let user = isExisted.toObject();
+    delete user.password;
+
+    return res.status(200).json({
+      message: "user loggedin successfully",
+      user,
     });
   } catch (error) {
     return res.status(500).json({
