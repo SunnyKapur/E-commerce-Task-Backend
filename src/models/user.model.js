319423bfb35985d13cda79baaaebb93f921a8b2c
SunnyKapur <kumarsunnykumar678@gmail.com>
2026-05-30 01:15:51 +0530

    Add user model schema

---
+++ src/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+let userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      minlength: 10,
+      maxlength: 10,
+      required: [true, "mobile is required"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+let UserModel = mongoose.model("user", userSchema);
+export default UserModel;
